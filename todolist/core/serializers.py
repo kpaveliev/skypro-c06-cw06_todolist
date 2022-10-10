690264f8e4b830e6b7ff402030348cd6a4379767
kpaveliev <kpaveliev@gmail.com>
2022-10-10 11:28:20 +0300

    field settings for signup

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -4,9 +4,13 @@ from .models import User
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
-
+    id = serializers.IntegerField(required=False)
     username = serializers.CharField(required=True, max_length=50)
+    first_name = serializers.CharField(required=False, allow_blank=True, max_length=50)
+    last_name = serializers.CharField(required=False, allow_blank=True, max_length=50)
+    email = serializers.EmailField(required=False)
+
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
