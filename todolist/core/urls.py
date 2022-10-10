f3d561a831d6a1d465c3718fb2b2723122355db2
kpaveliev <kpaveliev@gmail.com>
2022-10-10 10:52:22 +0300

    core urls registered

---
+++ todolist/core/urls.py
@@ -0,0 +1,14 @@
+# from django.urls import path, include
+# from djoser.views import UserViewSet
+# from rest_framework import routers
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+#
+#
+# users_router = routers.SimpleRouter()
+# users_router.register('users', UserViewSet)
+#
+# urlpatterns = [
+#     path('', include(users_router.urls)),
+#     path('token/', TokenObtainPairView.as_view()),
+#     path('refresh/', TokenRefreshView.as_view()),
+# ]
