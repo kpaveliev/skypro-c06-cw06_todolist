f04a1c581b95293642e14c0acda21a859970c5e2
kpaveliev <kpaveliev@gmail.com>
2022-10-11 20:29:44 +0300

    trailing slash removed

--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import SignUpView, LoginView, UserRetrieveUpdateView, PasswordUpdateView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('profile/', UserRetrieveUpdateView.as_view()),
-    path('update_password/', PasswordUpdateView.as_view())
+    path('signup', SignUpView.as_view()),
+    path('login', LoginView.as_view()),
+    path('profile', UserRetrieveUpdateView.as_view()),
+    path('update_password', PasswordUpdateView.as_view())
 ]
