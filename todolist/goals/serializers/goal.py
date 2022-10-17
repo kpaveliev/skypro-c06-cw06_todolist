e3b00cde77b2ac93f70caa9d9747d2d6bb63c60b
kpaveliev <kpaveliev@gmail.com>
2022-10-17 20:51:02 +0300

    goals app: goal category validation added

--- todolist/goals/serializers/goal.py
+++ todolist/goals/serializers/goal.py
@@ -7,17 +7,20 @@ from core.serializers import RetrieveUpdateSerializer
 
 class GoalCreateSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    category = serializers.SlugRelatedField(
-        source='category',
-        many=False,
-        queryset=Category.objects.all(),
-        slug_field='title'
-    )
 
     class Meta:
         model = Goal
         read_only_fields = ("id", "created", "updated", "user")
         fields = "__all__"
+
+    def validate_category(self, value):
+        if value.is_deleted:
+            raise serializers.ValidationError("not allowed in deleted category")
+
+        if value.user != self.context["request"].user:
+            raise serializers.ValidationError("not owner of category")
+
+        return value
 
 
 class GoalSerializer(serializers.ModelSerializer):
