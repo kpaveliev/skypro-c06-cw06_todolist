4d715c555d6a05797319fe6c0be57d15b95211be
kpaveliev <kpaveliev@gmail.com>
2022-10-22 18:51:07 +0500

    goals app: admin added

--- todolist/goals/admin.py
+++ todolist/goals/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from goals.models import Category
+from goals.models import Category, Goal
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,4 +8,10 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ("title", "user")
 
 
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "category", "created", "updated")
+    search_fields = ("title", "user", "category")
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Goal, CategoryAdmin)
--- todolist/goals/views/goal.py
+++ todolist/goals/views/goal.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import permissions, filters
+from rest_framework.pagination import LimitOffsetPagination
 
 from goals.models import Goal
 from goals.serializers import GoalCreateSerializer, GoalSerializer
