9afc4401356bb7e00479e3fe6b98f518b9bc4487
kpaveliev <kpaveliev@gmail.com>
2022-10-22 21:32:50 +0500

    goals app: mark goals as is_deleted when deleting parent category

--- todolist/goals/views/category.py
+++ todolist/goals/views/category.py
@@ -2,7 +2,7 @@ from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDe
 from rest_framework import permissions, filters
 from rest_framework.pagination import LimitOffsetPagination
 
-from goals.models import Category
+from goals.models import Category, Goal
 from goals.serializers import CategoryCreateSerializer, CategorySerializer
 from goals.permissions import UserPermissions
 
@@ -43,5 +43,11 @@ class CategoryView(RetrieveUpdateDestroyAPIView):
 
     def perform_destroy(self, instance):
         instance.is_deleted = True
+
+        goals = Goal.objects.filter(category=instance)
+        for goal in goals:
+            goal.is_deleted = True
+            goal.save()
+
         instance.save()
         return instance
