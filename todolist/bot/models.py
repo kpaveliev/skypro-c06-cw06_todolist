e73656a456d7bbcb027626349f39a6d55075be6c
kpaveliev <kpaveliev@gmail.com>
2022-11-02 11:30:20 +0300

    bot: TgUser model created

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from core.models import User
+
+
+class TgUser(models.Model):
+    class Meta:
+        verbose_name = "Пользователь телеграм"
+        verbose_name_plural = "Пользователи телеграм"
+
+    tg_user_id = models.IntegerField(verbose_name="ID пользователя в телеграм")
+    tg_chat_id = models.IntegerField(verbose_name="ID чата в телеграм")
+    user = models.ForeignKey(
+        User, verbose_name="Пользователь приложения", on_delete=models.PROTECT, null=True
+    )
