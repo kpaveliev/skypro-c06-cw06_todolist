810bddda5d8b56637d5eba3e7fa92bd3dc0f6c6f
kpaveliev <kpaveliev@gmail.com>
2022-11-03 10:51:12 +0300

    bot: telegram API dataclassed descriptions added

--- todolist/bot/tg/dc.py
+++ todolist/bot/tg/dc.py
@@ -3,10 +3,61 @@ from typing import List
 
 from marshmallow import EXCLUDE
 
+
+@dataclass
+class MessageFrom:
+    """Telegram API: https://core.telegram.org/bots/api#user"""
+    id: int
+    first_name: str
+    last_name: str
+    username: str
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+@dataclass
+class Chat:
+    """Telegram API: https://core.telegram.org/bots/api#chat"""
+    id: int
+    type: str
+    first_name: str
+    last_name: str
+    username: str
+    title: str
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+@dataclass
+class Message:
+    """Telegram API: https://core.telegram.org/bots/api#message"""
+    message_id: id
+    from_: MessageFrom  # "from" in Telegram API, underscore added
+    chat: Chat
+    text: str
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+@dataclass
+class UpdateObj:
+    """Telegram API: https://core.telegram.org/bots/api#getting-updates"""
+    update_id: int
+    message: Message
+    sender_chat: Chat
+
+    class Meta:
+        unknown = EXCLUDE
+
+
 @dataclass
 class GetUpdatesResponse:
+    """https://core.telegram.org/bots/api#getupdates"""
     ok: bool
-    result: List[UpdateObj]  # todo
+    result: List[UpdateObj]
 
     class Meta:
         unknown = EXCLUDE
@@ -14,8 +65,9 @@ class GetUpdatesResponse:
 
 @dataclass
 class SendMessageResponse:
+    """https://core.telegram.org/bots/api#sendmessage"""
     ok: bool
-    result: Message  # todo
+    result: Message
 
     class Meta:
         unknown = EXCLUDE
