cfa68743e3a56922423e4c2ce773a0280c58c81c
kpaveliev <kpaveliev@gmail.com>
2022-11-03 18:19:52 +0300

    bot-runbot: different message in case user already wrote or new

--- todolist/bot/management/commands/runbot.py
+++ todolist/bot/management/commands/runbot.py
@@ -3,6 +3,7 @@ from django.core.management import BaseCommand
 from bot.tg import TgClient
 
 from todolist.settings import TG_TOKEN
+from bot.models import TgUser
 
 
 class Command(BaseCommand):
@@ -15,6 +16,13 @@ class Command(BaseCommand):
         while True:
             response = tg_client.get_updates(offset=offset)
             for item in response.result:
+                # check if user exists in TgUser
+                tg_user_id = item.message.from_.id
+                chat_id = item.message.chat.id
                 offset = item.update_id + 1
 
-                tg_client.send_message(chat_id=item.message.chat.id, text=item.message.text)
+                if TgUser.objects.filter(tg_user_id=tg_user_id).first():
+                    tg_client.send_message(chat_id=chat_id, text='Существует')
+                else:
+                    TgUser.objects.create(tg_user_id=tg_user_id, tg_chat_id=chat_id)
+                    tg_client.send_message(chat_id=chat_id, text='Привет')
