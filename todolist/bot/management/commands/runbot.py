11367504eee2868f9a1783a02ab7507a142fd9e6
kpaveliev <kpaveliev@gmail.com>
2022-11-04 14:59:07 +0300

    bot-deploy: bot troubleshooting #3 - validation error arises periodically

--- todolist/bot/management/commands/runbot.py
+++ todolist/bot/management/commands/runbot.py
@@ -14,10 +14,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         offset = 0
         tg_client = TgClient(TG_TOKEN)
-        print(TG_TOKEN)
 
         while True:
             response = tg_client.get_updates(offset=offset)
+            print(response.status)
             for item in response.result:
                 offset = item.update_id + 1
 
